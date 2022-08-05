8bd2d2b5da717313af228282ee397939d201f522
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-08-05 21:26:16 +0300

    2 галерея

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const imgLists = galleryItems.map((image) =>`<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></div>`).join(" ");
+gallery.insertAdjacentHTML("afterbegin", imgLists);
+const instance = gallery.createElement(`<div class="modal"><img src="${image.original}"></div>`);
+gallery.insertAdjacentHTML("afterend", instance);
+
+
+const imageOpen = (event) => {
+    event.preventDefault();
+};
+gallery.addEventListener('click', imageOpen);
+
+
+// instance.show();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const imgLists = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a>`
+  )
+  .join(" ");
+gallery.insertAdjacentHTML("afterbegin", imgLists);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
