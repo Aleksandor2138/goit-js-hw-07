427bbf049066b9de36edbaf3050438cbbef02d17
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-08-07 15:53:43 +0300

    1 задача

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,19 +4,87 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-
+const body = document.querySelector("body");
+console.log(body);
 const gallery = document.querySelector(".gallery");
 
-const imgLists = galleryItems.map((image) =>`<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></div>`).join(" ");
+const imgLists = galleryItems.map(({preview, original, description }) =>`<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`).join(" ");
 gallery.insertAdjacentHTML("afterbegin", imgLists);
-const instance = gallery.createElement(`<div class="modal"><img src="${image.original}"></div>`);
-gallery.insertAdjacentHTML("afterend", instance);
 
+// Первый вариант решения задачи
 
-const imageOpen = (event) => {
-    event.preventDefault();
+// создание мобального окна и вложеных елементов
+const modal = document.createElement("div");
+modal.classList.add("modal");
+body.appendChild(modal);
+const placeholder = document.createElement("div");
+placeholder.classList.add("basicLightbox__placeholder");
+placeholder.setAttribute("role", "dialog");
+modal.appendChild(placeholder);
+
+const images = document.querySelectorAll(".gallery__image");
+
+images.forEach(image => {
+    const imageOpen = (event) => {
+        // отмена действий браузера по  умолчанию 
+        event.preventDefault();
+        // добавление классов basicLightbox + тег фото
+        modal.classList.add("basicLightbox", "basicLightbox--img", "basicLightbox--visible");
+        const img = document.createElement("img");
+        img.src = image.dataset.source;
+        while (placeholder.firstChild) {
+            placeholder.removeChild(placeholder.firstChild);
+        }
+        placeholder.appendChild(img);
+
+    }
+    console.log(image.dataset.source);
+    image.addEventListener('click', imageOpen);
+});
+
+const imageClose = (event) => {
+    if (event.target !== event.currentTarget) return;
+    modal.classList.remove("basicLightbox", "basicLightbox--img", "basicLightbox--visible");
+        console.log(event.target);
 };
-gallery.addEventListener('click', imageOpen);
+modal.addEventListener("click", imageClose);
+//  Закрытие с кнопки escape
+const pressKeyLightBox = (event) => {
+    if (modal.classList.contains("basicLightbox")){
+        if (event.code === "Escape") {
+            modal.classList.remove("basicLightbox", "basicLightbox--img", "basicLightbox--visible");
+        }
+        console.log(`соблюдение первого if`)
+    }
+}
+body.addEventListener("keyup", pressKeyLightBox);
 
 
-// instance.show();
+
+//  Второй вариант реализации задачи
+
+// const images = document.querySelectorAll(".gallery__image");
+// images.forEach(image => {
+//     const imageOpen = (event) => {
+//         // отмена действий браузера по  умолчанию 
+//         event.preventDefault();
+//         // инициализация basicLightbox базовый с фото
+//         const instance = basicLightbox.create(`<img class="img-modal" src="${image.dataset.source}" >`);
+//         instance.show();
+//     }
+//     console.log(image.dataset.source);
+//     image.addEventListener('click', imageOpen);
+// });
+// //  Закрытие с кнопки escape
+// const pressKeyLightBox = (event) => {
+//     const modal = document.querySelector(".basicLightbox");
+//     console.log(modal)
+//     if (modal.classList.contains("basicLightbox")){
+//         if (event.code === "Escape") {
+//             modal.classList.remove("basicLightbox", "basicLightbox--img", "basicLightbox--visible");
+//         }
+//         console.log(`dsgjkytyj`)
+//     }
+
+// }
+// body.addEventListener("keyup", pressKeyLightBox);
