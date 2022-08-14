17a27a1301d84211e8010d8b25b1573df93b1723
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-08-14 11:57:38 +0300

    правки симантика

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ const gallery = document.querySelector(".gallery");
 const imgLists = galleryItems
   .map(
     (image) =>
-      `<a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a>`
+      `<li><a  href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a></li>`
   )
   .join(" ");
 gallery.insertAdjacentHTML("afterbegin", imgLists);
